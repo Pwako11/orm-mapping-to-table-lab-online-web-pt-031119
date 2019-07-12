db40c673192e926d7e3fd974129a5b307fb5b56f
Patrick A <akolo@hotmail.com>
2019-07-12 00:18:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
   end 
   
   def self.create_table
-    sql = <<- SQL 
+    sql =  <<-SQL
       CREATE TABLE IF NOT EXISTS students (
         id INTERGER PRIMARY KEY, 
         name TEXT,
@@ -24,7 +24,7 @@ class Student
   end 
   
   def self.drop_table
-    sql = <<- SQL 
+    sql =  <<-SQL 
       DELETE FROM studets WHERE name = ?
       DELETE FROM students WHERE grade = ?
       SQL 
@@ -32,7 +32,7 @@ class Student
   end
   
   def save
-    sql = <<- SQL 
+    sql =  <<-SQL 
     INSERT INTO students (name, grade) 
     VALUES (?, ?)
     SQL
